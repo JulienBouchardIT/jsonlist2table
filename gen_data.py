adc10f2323ab5345f49a3e7ed492310a54568141
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2023-05-28 16:07:23 -0400

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -1,3 +1,3 @@
-f = open("data.json", "w")
+f = open("page/data.json", "w")
 f.write('[{"id":"123","name":"bob","email":"bob@gmail.com"},{"id":"456","name":"alice","email":"alice@gmail.com"}]')
 f.close()
