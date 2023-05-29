5c1e97495611fd9c4b04f22bbc2797eb0965a977
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2023-05-29 00:08:53 -0400

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -1,3 +1,27 @@
+import random
+import json
+
+# Generate a random list of objects
+def generate_random_objects(num_objects):
+    objects = []
+    for _ in range(num_objects):
+        obj = {
+            "id": random.randint(1000, 1999),
+            "firstname": random.choice(["John", "Alice", "Bob", "Jane"]),
+            "lastname": random.choice(["Doe", "Smith", "Johnson", "Williams"])
+        }
+        objects.append(obj)
+    return objects
+
+# Number of objects to generate
+num_objects = 5
+
+# Generate random objects
+objects_list = generate_random_objects(num_objects)
+
+# Convert the list of objects to JSON
+json_data = json.dumps(objects_list, indent=4)
+
 f = open("page/data.json", "w")
-f.write('[{"id":"123","name":"bob","email":"bob@gmail.com"},{"id":"456","name":"alice","email":"alice@gmail.com"}]')
+f.write(json_data)
 f.close()
